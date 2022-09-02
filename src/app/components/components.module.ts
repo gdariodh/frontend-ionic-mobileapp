53921b776d92d898183da8a147dd76fab0b0883a
gdariodh <diazhernandezgabriel2001@gmail.com>
2022-09-02 00:14:33 -0400

    separar noticias en articles

---
+++ src/app/components/components.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ArticlesComponent } from './articles/articles.component';
+import { IonicModule } from '@ionic/angular';
+
+
+
+@NgModule({
+  declarations: [
+    ArticlesComponent
+  ],
+  imports: [
+    CommonModule,
+    IonicModule
+  ],
+  exports:[
+    ArticlesComponent
+  ]
+})
+export class ComponentsModule { }
